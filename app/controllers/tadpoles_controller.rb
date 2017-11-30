ba4cafe1ddf5e3a75a70bac41fa53f2675f0d99c
John Tuttle <thejwt@gmail.com>
2017-11-30 22:51:02 +0000

    automatically backed up by learn

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -5,7 +5,7 @@ class TadpolesController < ApplicationController
     if @tadpole = Tadpole.find_by(id: params[:id])
       if @frog = Frog.create(name: @tadpole.name, color: @tadpole.color, pond_id: @tadpole.pond.id)
         @tadpole.destroy
-        redirect_to frog_path(@frong)
+        redirect_to frog_path(@frog)
       else
       end
     else
