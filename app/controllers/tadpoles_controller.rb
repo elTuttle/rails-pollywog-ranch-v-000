8b3978187c81cf1ea3930787a1455a32eb99be4c
John Tuttle <thejwt@gmail.com>
2017-11-30 22:45:28 +0000

    automatically backed up by learn

--- app/controllers/tadpoles_controller.rb
+++ app/controllers/tadpoles_controller.rb
@@ -1,7 +1,9 @@
 class TadpolesController < ApplicationController
   before_action :set_tadpole, only: [:show, :edit, :update, :destroy, :metamorphosize]
 
-  # add your metamorphosize action here
+  def metamorphosize
+    
+  end
   
   def index
     @tadpoles = Tadpole.all
